9ae4040366ec77dafb07d984080e3fdad0aa738c
Ayan7864 <ayanshilledar786@gmail.com>
2026-01-05 11:42:52 +0530

    employee project

--- test_employee.py
+++ test_employee.py
@@ -5,3 +5,13 @@ def test_get_employee_details():
     emp_id = "E001"
     department = "IT"
     salary = 50000
+   
+ expected_output = (
+        "Employee Name: John\n"
+        "Employee ID: E001\n"
+        "Department: IT\n"
+        "Salary: 50000"
+    )
+actual_output = get_employee_details(name, emp_id, department, salary)
+
+ assert actual_output == expected_output
